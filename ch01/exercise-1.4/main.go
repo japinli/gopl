3a49e2eb9fb725909528910c4d3cf072d81c9314
japinli <japinli@hotmail.com>
2021-08-28 20:10:12 +0800

    Add solution for exercise 1.4

---
+++ ch01/exercise-1.4/main.go
@@ -0,0 +1,55 @@
+// Modify dup2 to print the names of all files in which each duplicated
+// line occurs.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	foundIn := make(map[string][]string)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, foundIn)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts, foundIn)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%v\t%s\n", n, foundIn[line], line)
+		}
+	}
+}
+
+func in(name string, filenames []string) bool {
+	for _, s := range filenames {
+		if name == s {
+			return true
+		}
+	}
+
+	return false
+}
+
+func countLines(f *os.File, counts map[string]int, foundIn map[string][]string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		line := input.Text()
+		counts[line]++
+		if !in(f.Name(), foundIn[line]) {
+			foundIn[line] = append(foundIn[line], f.Name())
+		}
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
